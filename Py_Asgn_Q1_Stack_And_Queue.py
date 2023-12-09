94ed4f40815422ddf30dcaf09c59a2872d5c5ee9
Bhavy Doshi <doshibhavy21@gmail.com>
2023-12-09 13:36:02 -0400

    Initial test case for Q1

---
+++ Py_Asgn_Q1_Stack_And_Queue.py
@@ -0,0 +1,21 @@
+
+
+new_list = [1,2,3,4]
+print("Adding new element to the stack")
+new_list=stack(new_list, 'add', 0)
+print(new_list)
+
+new_list = [1,2,3,4]
+print("Adding remove element from the stack")
+new_list=stack(new_list, 'remove', 0)
+print(new_list)
+
+new_list = [1,2,3,4]
+print("Adding new element to the queue")
+new_list=queue(new_list, 'add', 0)
+print(new_list)
+
+new_list = [1,2,3,4]
+print("Adding remove element from the queue")
+new_list=queue(new_list, 'remove', 0)
+print(new_list)
